7ea5c6246c3af6bd9f1b218d4c83271aad7cf920
shihyuhuang <61773397+shihyuhuang@users.noreply.github.com>
2023-04-30 23:53:07 +0800

    Add files via upload

--- HW3/r10943181_hw3.m
+++ HW3/r10943181_hw3.m
@@ -1,5 +1,13 @@
-I = imread('peppers.png');
+clear;
+clc;
+
+I = imread('firework.jpeg');
 img = im2double(I);
+
+figure(1)
+image(img);
+title('original');
+
 R = img(:,:,1);
 G = img(:,:,2);
 B = img(:,:,3);
@@ -13,7 +21,64 @@ Cb = matrix(2,1)*R+matrix(2,2)*G+matrix(2,3)*B;
 Cr = matrix(3,1)*R+matrix(3,2)*G+matrix(3,3)*B;
 
 % 4:2:0
+Y_com = Y;
+Cb_com = Cb(1:2:end,1:2:end);
+Cr_com = Cr(1:2:end,1:2:end);
 
-Cb = Cb(1:2:end,1:2:end);
-Cr = Cr(1:2:end,1:2:end);
+% reconstruct
+% 1
+Y_re = Y_com;
 
+Cb_re_temp = [];
+Cr_re_temp = [];
+for i=1:size(Cb_com,1)
+    Cb_re_temp = [Cb_re_temp;Cb_com(i,:);ones(1,size(Cb_com,2))];
+    Cr_re_temp = [Cr_re_temp;Cr_com(i,:);ones(1,size(Cr_com,2))];
+end
+
+Cb_re = [];
+Cr_re = [];
+for i=1:size(Cb_com,2)
+    Cb_re = [Cb_re,Cb_re_temp(:,i),ones(2*size(Cb_com,1),1)];
+    Cr_re = [Cr_re,Cr_re_temp(:,i),ones(2*size(Cr_com,1),1)];
+end
+
+% 2
+for m=1:size(Cb_com,1)-1
+    Cb_re(2*m,:) = (Cb_re(2*m-1,:)+Cb_re(2*m+1,:))/2;
+    Cr_re(2*m,:) = (Cr_re(2*m-1,:)+Cr_re(2*m+1,:))/2;
+end
+
+if mod(size(Cb_re,1),2)==0
+    Cb_re(end,:) = Cb_re(end-1,:);
+    Cr_re(end,:) = Cr_re(end-1,:);
+end
+
+for m=1:size(Cb_com,2)-1
+    Cb_re(:,2*m) = (Cb_re(:,2*m-1)+Cb_re(:,2*m+1))/2;
+    Cr_re(:,2*m) = (Cr_re(:,2*m-1)+Cr_re(:,2*m+1))/2;
+end
+
+if mod(size(Cb_re,2),2)==0
+    Cb_re(:,end) = Cb_re(:,end-1);
+    Cr_re(:,end) = Cr_re(:,end-1);
+end
+
+matrix_inv = inv(matrix);
+
+R_af = matrix_inv(1,1)*Y_re+matrix_inv(1,2)*Cb_re+matrix_inv(1,3)*Cr_re; 
+G_af = matrix_inv(2,1)*Y_re+matrix_inv(2,2)*Cb_re+matrix_inv(2,3)*Cr_re; 
+B_af = matrix_inv(3,1)*Y_re+matrix_inv(3,2)*Cb_re+matrix_inv(3,3)*Cr_re;
+
+img_af = ones(size(img));
+img_af(:,:,1) = R_af;
+img_af(:,:,2) = G_af;
+img_af(:,:,3) = B_af;
+
+figure(2);
+image(img_af);
+title('reconstruct');
+
+MAX=1;
+MES=sum(sum(sum((img-img_af).^2)))/(size(img,1)*size(img,2)*3);    %均方差
+PSNR=20*log10(MAX/sqrt(MES)) 
